3e4f0a585bf1b2fc02ffc9c821fdf6c7fdf7b43f
Heavenell <Fauzirahmadiariateja@gmail.com>
2026-05-19 18:05:35 +0200

    Create /page
    to see how next.js works

---
+++ app/game/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  const name = "Heavenell";
+
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="w-full max-w-2xl p-6">
+        
+        <header className="text-center mb-10">
+          <h1 className="text-4xl font-bold">GAME</h1>
+        </header>
+
+      </main>
+    </div>
+  );
+}
